d69e0c371ccce60ca222ae45e8599312bc213f71
hari charan <charangattamaneni@gmail.com>
2025-05-18 19:40:21 +0530

    hello worls stared

---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "cbn:: hey jagu, how are you?"
+echo "jagu:: i am fine too,please dont arrest my wife in liquor scam"
+echo "cbn:: ok jagu , i will not arrest ur wife."
+echo "jagu:: thank you cbn"
