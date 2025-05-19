2d55ca1f0410fc8e4030d36e3a30f84097ea29b0
hari charan <charangattamaneni@gmail.com>
2025-05-19 11:35:22 +0530

    hello worls stared

---
+++ 06-arguments.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+person1=$1
+person2=$2
+
+echo "$person1:: hey $person2, how are you?"
+echo "$person2:: i am fine too,please dont arrest my wife in liquor scam"
+echo "$person1:: ok $person1 , i will not arrest ur wife."
+echo "$person2:: thank you cbn"
+
