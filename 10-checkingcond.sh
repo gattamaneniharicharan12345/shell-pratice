8693d72dcecdae8f04bfcb1f7aeeafa28d94d8cf
hari charan <charangattamaneni@gmail.com>
2025-05-20 23:29:41 +0530

    hello worls stared

---
+++ 10-checkingcond.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# USERID=$(id -u)
+
+# if [ $USERID -ne 0 ]
+# then
+#     echo "ERROR:: Please run this script with root access"
+#     exit 1 #give other than 0 upto 127
+# else
+#     echo "You are running with root access"
+# fi
+
+# dnf list installed mysql
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then
+    echo "Error:: please run this scripit with root access"
+else
+    echo "Your running with root access:"
+fi    
