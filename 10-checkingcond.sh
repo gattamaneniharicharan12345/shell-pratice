8f455eb8d3faf649c01af944f9b925b0eae7c037
hari charan <charangattamaneni@gmail.com>
2025-05-20 23:43:43 +0530

    hello worls stared

--- 10-checkingcond.sh
+++ 10-checkingcond.sh
@@ -23,3 +23,11 @@ else
 fi    
 
 dnf install mysql -y
+
+if [ $? -eq 0 ]
+then
+    echo "Installing MySQL is ... SUCCESS"
+else
+    echo "Installing MYSQL is ... Failure"
+    exit 1
+fi           
