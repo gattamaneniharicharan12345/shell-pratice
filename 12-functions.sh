4d2a00e4dbf9a3618bc6d5f3f9d30fd389cef2b7
hari charan <charangattamaneni@gmail.com>
2025-05-21 23:04:59 +0530

    conditionsstatements

---
+++ 12-functions.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then
+    echo "Error:: please run this scripit with root access"
+    exit 1
+else
+    echo "Your running with root access:"
+fi   
+
+
+VALIDATE(){
+    if [ $1 -eq 0 ]
+then
+    echo "Installing $2 is ... SUCCESS"
+else
+    echo "Installing $2 is ... Failure"
+    exit 1
+fi
+}
+
+dnf list installed mysql
+if [ $? -ne 0 ]
+then
+    echo "MySQL is not installed... going to install it"
+    dnf install mysql -y
+    VALIDATE $? "mysql"
+else
+    echo "MySQL is already installed...Nothing to do"
+fi
+
+dnf list installed python3
+if [ $? -ne 0 ]
+then
+    echo "python3 is not installed... going to install it"
+    dnf install python3 -y
+    VALIDATE $? "python3"
+else
+    echo "python3 is already installed...Nothing to do"
+fi
+
+dnf list installed ngnix
+if [ $? -ne 0 ]
+then
+    echo "ngix is not installed... going to install it"
+    dnf install n -y
+    VALIDATE $? "nginx"
+else
+    echo "ngnix is already installed...Nothing to do"
+fi
