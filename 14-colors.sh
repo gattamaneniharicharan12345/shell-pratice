f58f8be13a5a3c953857cc839af704173fad4222
hari charan <charangattamaneni@gmail.com>
2025-05-24 10:37:00 +0530

    colors

---
+++ 14-colors.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]
+then
+    echo "Error:: please run this scripit with root access"
+    exit 1
+else
+    echo "Your running with root access:"
+fi        
+
+VALIDATE(){
+    if [ $1 -eq 0 ]
+then
+    echo "Installing $2 is ... SUCCESS"
+else
+    echo "Installing $2 is ... Failure"
+    exit 1
+fi        
+}
+
+dnf list installed mysql
+if [ $? -ne 0 ]
+then
+    echo "MySQL is not installed... going to install it"
+    dnf install mysql -y
+else
+    echo "Mysql is installed.... nothing todo"
+fi   
+ dnf list installed nginx
+
+If [ $? -ne 0 ]
+then
+    echo "nginx is not installed... going to install it"
+    dnf install nginx -y
+else
+    echo "nginx is installed... nothing to do"
+fi
+
+dng list installed python3
+
+if [ &? -ne 0 ]
+then
+    echo "Python3 is not installed... going to install it"
+    dnf install python3 -y
+else
+    echo "Python3 is installed.. noyhing to do"
+fi    
